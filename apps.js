6c7213db1eb55fd5fdcb81807a34ea736c19589c
scarter86 <samuelcarter1@gmail.com>
2023-06-14 16:05:27 +0100

    number quiz finished and styled

--- apps.js
+++ apps.js
@@ -2,3 +2,29 @@ console.log("name")
     let username = prompt("Hello!  What is your name? ");
     let usernameElement = document.getElementById("name");
     usernameElement.innerText = username
+
+    let y = Math.floor(Math.random() * 10 + 1);
+ 
+    
+    let guess = 1;
+
+    document.getElementById("submitguess").onclick = function () {
+
+    
+        let x = document.getElementById("guessField").value;
+
+        if (x == y) {
+            alert("YOU ARE A GENIUS OF LEVELS WE HAVEN'T SEEN BEFORE"
+                + guess + " GUESS ");
+        }
+
+        
+        else if (x > y) {
+            guess++;
+            alert("SORRY! TRY A SMALLER NUMBER");
+        }
+        else {
+            guess++;
+            alert("OOPS SORRY!! TRY A GREATER NUMBER")
+        }
+    }
